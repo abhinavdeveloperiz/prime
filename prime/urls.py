7645f7e6afc3fb53cbe8c3b4153018a385cb692f
abhinavmabhi <dexmarketing192018@gmail.com>
2025-12-11 16:23:47 +0530

    first

---
+++ prime/urls.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from app import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("",views.home,name="home"),
+    path("about/",views.about,name="about"),
+    path("services/",views.services,name="service"),
+    path("gallery/",views.gallery,name="gallery"),
+    path("contact/",views.contact,name="contact"),
+    
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
