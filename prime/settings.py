352f5af016805d5b121588c50de1fde75cdb7bb8
abhinavmabhi <dexmarketing192018@gmail.com>
2026-02-11 17:03:22 +0530

    debug ture

--- prime/settings.py
+++ prime/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0fa6@f7lg)sz4o38&d^aejxeuiqaisbnava_ar*3j#x7!q!umt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "primehub.up.railway.app",
