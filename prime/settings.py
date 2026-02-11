0dd984e22a84595de528e9ba2cfa4b4a57ecd20f
abhinavmabhi <dexmarketing192018@gmail.com>
2026-02-11 17:01:26 +0530

    commit with static

--- prime/settings.py
+++ prime/settings.py
@@ -142,17 +142,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-import os
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"] 
-STATIC_ROOT = BASE_DIR / "staticfiles"    
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / "media"          
+MEDIA_ROOT = BASE_DIR / "media"         
 
 
 
